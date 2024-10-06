63d7779465b2759514290c87ede3d5cea5360c58
Remy Cantrelle <remycantrelle@9c-3e-53-82-06-8a.wlan.lsu.edu>
2024-10-05 21:17:44 -0500

    Wall Drawing 164

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(500, 500);
+    background(200);
+}
+
+       function draw(){
+       rect(30, 20, 440, 450);
+stroke('red');
+       
+       
+       translate(155, 150);
+ line(270, 95, -75, 95);
+ stroke('black');
+
+rotate(900, 62)
+stroke('red');
+line(30, -170, 180, -4);
+stroke('black');
+       }
+       
+
+
